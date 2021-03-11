44b33e3900f6148660c312fe4b05b1df612d4d27
Johannes Gorset <johannes.gorset@schibsted.com>
2021-03-11 23:06:06 +0100

    Rearrange modules

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Deathstar = void 0;
+var middleware_1 = require("./middleware");
+Object.defineProperty(exports, "Deathstar", { enumerable: true, get: function () { return middleware_1.Deathstar; } });
